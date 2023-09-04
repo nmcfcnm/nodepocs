948ff1f385b31f5c37036ec27a590e3068690b36
Malhotra <NMalhot6@its.jnj.com>
2023-09-05 02:28:43 +0530

    Updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Azure App Service!\n');
+  res.end('Hello, Azure App Service! I am a dev whose name is Nitin Malhotra\n');
 });
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
